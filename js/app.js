00ddeaeeee6d9cf1f78db20d1337b286908ec26d
Adrien Espel <espel.adrien@gmail.com>
2015-04-15 23:15:41 +0200

    Init

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Adrien on 15/04/2015.
+ */
+var React = require('react');
+
+var App = React.createClass({
+    getInitialState: function(){
+        this.state.data = { title: 'Just wow' };
+    },
+    render: function() {
+        return(
+            <div className="row">
+                <Welcome data={this.state.data}/>
+            </div>
+        );
+    }
+});
+
+var Welcome = React.createClass({
+    getInitialState: function(){
+        this.state = this.props.data;
+    },
+    handleWelcomeClick: function(e) {
+        console.log('log', this.state.data);
+    },
+    render: function() {
+        return(
+            <div className="small-12.columns">
+                <h1>{this.state.title}</h1>
+                <button onClick={this.handleWelcomeClick}>Welcome</button>
+            </div>
+        );
+    }
+});
+
+React.render(
+    <App />,
+    document.getElementById('app')
+);
